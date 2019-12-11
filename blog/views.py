f0d33d8ad012798863c1a335d7264b55dd7288de
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-11 14:34:59 +0900

    191211 post model operates in admin

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return HttpResponse('<html><body><h1>Post List!</h1></body></html>')
