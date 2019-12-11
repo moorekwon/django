5c22e364249062001a100edf014b3ba8464ce29a
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-11 14:42:08 +0900

    191211 return html texts from post_list view

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    return HttpResponse('<html><body><h1>Post List!</h1></body></html>')
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List!</h1>'
+        '<div>'
+        '<p>published: 12.11.2019, 14.:38</p>'
+        '<h2><a href="">My First post</a></h2>'
+        '<p>Hello</p>'
+        '</div>'
+        '<p>published: 12.11.2019, 14.:39</p>'
+        '<h2><a href="">My Second post</a></h2>'
+        '<p>Hi</p>'
+        '</div>'
+        '</body>'
+        '</html>'
+    )
