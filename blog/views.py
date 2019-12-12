8fc0eb4133a13895add0ff33b6dc7df3a7ebb9ea
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-12 13:22:20 +0900

    191211 tried to add post-detail (but failed..)

--- blog/views.py
+++ blog/views.py
@@ -34,12 +34,19 @@ def post_list(request):
     # return HttpResponse(html)
 
 
-
     posts = Post.objects.all()
     context = {
         'posts': posts
     }
 
     # Template을 찾을 경로에서 post_list.html을 찾아 text로 만들고 HttpResponse 형태로 돌려줌
-    # shortcut 함수
+    # shortcut 함수post_detail
     return render(request, 'post_list.html', context)
+
+
+def post_detail(request):
+    # URL: /post-detail/
+    # View: post_detail
+    # Template: post_detail.html
+    # 내용: <h1>Post Detail!</h1>
+    return render(request, 'post_detail.html')
