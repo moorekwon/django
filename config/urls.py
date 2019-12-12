1bb4966ac302636a6faf638c4915f12a358889cc
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-12 16:02:58 +0900

    191212 used url tag

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from blog.views import post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('post-list/', post_list),
-    path('post-detail/<int:pk>/', post_detail, name='post_detail')
+    path('posts/', post_list, name='url-name-post-list'),
+    path('post-detail/<int:pk>/', post_detail, name='url-name-post-detail')
 ]
