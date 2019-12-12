eeb0391d67ff3f38bcaa18f134239ccfab3c7d0d
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-12 13:47:08 +0900

    191211 post_detail shows Post[0]

--- blog/views.py
+++ blog/views.py
@@ -34,13 +34,17 @@ def post_list(request):
     # return HttpResponse(html)
 
 
+    # posts라는 변수에 전체 Post를 갖는 QuerySet 객체를 전달
+    # Post.objects.무언가...를 실행한 결과는 QuerySet 객체가 됨
+    # context라는 dict를 생성하며 'posts' 키에 위 posts 변수를 value로 사용하도록 함
+    # render의 3번째 위치인자로 위 context 변수를 전달
     posts = Post.objects.all()
     context = {
         'posts': posts
     }
 
     # Template을 찾을 경로에서 post_list.html을 찾아 text로 만들고 HttpResponse 형태로 돌려줌
-    # shortcut 함수post_detail
+    # shortcut 함수
     return render(request, 'post_list.html', context)
 
 
@@ -48,5 +52,15 @@ def post_detail(request):
     # URL: /post-detail/
     # View: post_detail
     # Template: post_detail.html
-    # 내용: <h1>Post Detail!</h1>
-    return render(request, 'post_detail.html')
+
+    # 전체 Post 목록(Post 전체 QuerySet) 중 [0] index에 해당하는 Post 객체 하나를 post 변수에 할당
+    # 'context'라는 이름의 dict를 만들어 'post' key 위에 위 post 변수를 value로 사용
+    # 이 context 변수를 render의 3번째 인자로 전달
+    # post_detail.html에서는 전달받은 'post' 변수의 title, author, text, created_date, published_date를 출력
+    post = Post.objects.all()[0]
+    context = {
+        'post': post
+    }
+
+
+    return render(request, 'post_detail.html', context)
